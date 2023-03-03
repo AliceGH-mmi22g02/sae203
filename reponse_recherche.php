44304bc0be0f84295651e3b15f4afb4ec8e6efd7
Ghantfasia <aliceghanimi@hotmail.com>
2023-03-03 13:22:04 +0000

    Comise a jour de reponse_recherche

--- reponse_recherche.php
+++ reponse_recherche.php
@@ -10,16 +10,15 @@ if (count($_GET)==0) {
 // Vérification des données du formulaire
 if (!empty($_GET["recherche"])) {
   $recherche_nettoye = filter_var( $_GET["recherche"] , FILTER_SANITIZE_STRING );
-  if($recherche_nettoye==null) { $_SESSION['information'] = '<h4 style="color:red;">*Champ non valide </h4>'; }
+  if($recherche_nettoye==null) { $_SESSION['information'] = '<h4 style="color:red;">*Champ non valide </h4>'; header('location: form_recherche.php'); }
   else {
     $_SESSION['information']=$recherche_nettoye;
-    
+    header('location: listing.php');
   }
 
 } else {
   $_SESSION['information'] ='<h4 style="color:red;">*Veuillez compléter ce champ</h4>';
+  header('location: form_recherche.php');
 }
-
-header('location: form_recherche.php');
 
 ?>
